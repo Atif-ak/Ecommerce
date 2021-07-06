1a1da7db528e5776332525c4b8d5e48abeff0063
Atif-ak <81744577+Atif-ak@users.noreply.github.com>
2021-07-06 13:25:49 +0530

    Create Gaming.js

---
+++ src/components/homePage/Gaming.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CardHome from '../Card/CardHome'
+
+function Gaming() {
+    return (
+        <div>
+           <CardHome/>
+        </div>
+    )
+}
+
+export default Gaming
